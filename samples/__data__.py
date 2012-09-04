0b5689de13c67e2cfc606c1fc043913d2f49d2b8
Andrzej Zuranski <zuranski@della3.(none)>
2012-09-04 09:53:02 -0400

    removed unneeded signal MC samples from dict.

--- samples/__data__.py
+++ samples/__data__.py
@@ -12,6 +12,6 @@ data.add('dataA','%s/%s/%s")'%(pnfs,loc,"DataRun2011A") ,lumi=0.5725) #/pb
 '''
 
 #PU
-loc='/tigress-hsm/zuranski/work/cms/data/DJTuple44x_PROD/'
+loc='/tigress-hsm/zuranski/work/cms/data/DJTuple44x_DEV/'
 data.add('dataA','%s'%(fileListFromDisk(loc+"/DataRun2011A")),lumi=0.5725) #/pb
 data.add('dataB','%s'%(fileListFromDisk(loc+"/DataRun2011B")),lumi=0.5725) #/pb
