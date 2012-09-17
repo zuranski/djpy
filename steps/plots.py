423097c8d124b6f9f329990a7bf5d9effe2aa55b
Andrzej Zuranski <zuranski@della3.(none)>
2012-09-17 09:17:21 -0400

    added ABCD plots.

--- steps/plots.py
+++ steps/plots.py
@@ -2,25 +2,49 @@ from supy import analysisStep
 import itertools
 
 class plots(analysisStep):
-	def __init__(self,njets=2,indices=None):
+	def __init__(self,njets=2,indices=None,plot2D=False,plot1D=True):
 		self.prefix = ['jet','dijet'][njets-1]
 		self.indices = indices if indices is not None else self.prefix+'Indices'
+		self.plot1D = plot1D
+		self.plot2D = plot2D
 
 	def uponAcceptance(self,e):
-		for var in self.vars:
-			for idx in e[self.indices]:
-				self.book.fill(e[self.prefix+var['name']][idx],var['name']+'_h',var['bins'],var['low'],
-							   var['high'],w=None,title="%s; %s%s ; %s / bin "
-							   %(self.prefix,var['name'],var['unit'],self.prefix))
+		if self.plot1D:
+			for var in self.vars:
+				for idx in e[self.indices]:
+					self.book.fill(e[self.prefix+var['name']][idx],var['name']+'_h',var['bins'],var['low'],
+								   var['high'],w=None,title="%s; %s%s ; %s / bin "
+								   %(self.prefix,var['name'],var['unit'],self.prefix))
 
-		if hasattr(self,'correlations'):
+		if self.plot2D:
 			for var1,var2 in itertools.combinations(self.vars,2):
 				for idx in e[self.indices]:
 					self.book.fill((e[self.prefix+var1['name']][idx],e[self.prefix+var2['name']][idx]),
                                    var1['name']+'_'+var2['name']+'_h',(var1['bins'],var2['bins']),
                                    (var1['low'],var2['low']),(var1['high'],var2['high']),
-								   w=None,title="%s%s; %s%s; / bin" 
-								   %(var1['name'],var1['unit'],var2['name'],var2['unit']))
+								   w=None,title="%s ; %s%s; %s%s" 
+								   %(self.prefix,var1['name'],var1['unit'],var2['name'],var2['unit']))
+
+class ABCDplots(analysisStep):
+	def __init__(self,indices=None):
+		self.indices = indices
+		self.collection=indices[:indices.find('_ABCDIndices')]+indices[indices.find('_ABCDIndices')+12:]
+		self.cut1,self.cut2 = indices.split('_')[:2]
+
+	def uponAcceptance(self,e):
+		indicesDict = e[self.indices]
+		self.book.fill((0,0),self.collection+'counts',(2,2),(-0.5,-0.5),(1.5,1.5),w=len(indicesDict['A']),
+		title=self.collection + " ; " + self.cut1 +";"+ self.cut2+"; counts",
+		xAxisLabels=['False','True'],yAxisLabels=['False','True'])
+		self.book.fill((0,1),self.collection+'counts',(2,2),(-0.5,-0.5),(1.5,1.5),w=len(indicesDict['B']),
+		title=self.collection + " ; " + self.cut1 +";"+ self.cut2+"; counts",
+		xAxisLabels=['False','True'],yAxisLabels=['False','True'])
+		self.book.fill((1,0),self.collection+'counts',(2,2),(-0.5,-0.5),(1.5,1.5),w=len(indicesDict['C']),
+		title=self.collection + " ; " + self.cut1 +";"+ self.cut2+"; counts",
+		xAxisLabels=['False','True'],yAxisLabels=['False','True'])
+		self.book.fill((1,1),self.collection+'counts',(2,2),(-0.5,-0.5),(1.5,1.5),w=len(indicesDict['D']),
+		title=self.collection + " ; " + self.cut1 +";"+ self.cut2+"; counts",
+		xAxisLabels=['False','True'],yAxisLabels=['False','True'])
 
 
 class general(plots):
@@ -60,9 +84,8 @@ class vertices(plots):
             {'name':'VtxN','bins':15,'low':1.5,'high':16.5,'unit':''},
             {'name':'Vtxmass','bins':50,'low':0,'high':100,'unit':'[GeV]'},
             {'name':'Vtxpt','bins':50,'low':0,'high':200,'unit':'[GeV]'},
-            {'name':'VtxptRatio','bins':50,'low':0,'high':1,'unit':''},
-            {'name':'VtxNTotRatio','bins':20,'low':0,'high':1,'unit':''},
-            {'name':'Posip2dFrac','bins':10,'low':0,'high':1,'unit':''},
+            {'name':'VtxNRatio','bins':20,'low':0,'high':1,'unit':''},
+            {'name':'Posip2dFrac','bins':10,'low':0,'high':1.001,'unit':''},
             {'name':'NAvgMissHitsAfterVert','bins':6,'low':0,'high':6,'unit':''},
            ]
 
@@ -72,9 +95,23 @@ class clusters(plots):
             {'name':'bestclusterN','bins':15,'low':1.5,'high':16.5,'unit':''},
            ]
 
-class discriminants(plots):
-	correlations=True
+class ABCDvars(plots):
 	vars = [
-            {'name':'DiscriminantVtx','bins':50,'low':0.99,'high':1,'unit':''},
-            {'name':'DiscriminantPromptness','bins':50,'low':0.99,'high':1,'unit':''},
-           ]    
+            {'name':'Discriminant','bins':50,'low':0.,'high':1,'unit':''},
+            {'name':'Promptness','bins':50,'low':0.,'high':20,'unit':''},
+            {'name':'Mass','bins':50,'low':0,'high':500,'unit':'[GeV]'},
+           ]
+
+class cutvars(plots):
+	vars = [
+            {'name':'Mass','bins':50,'low':0,'high':500,'unit':'[GeV]'},
+            {'name':'Promptness','bins':50,'low':0,'high':20,'unit':''},
+            {'name':'Lxysig','bins':50,'low':0,'high':100,'unit':''},
+            {'name':'VtxN','bins':15,'low':1.5,'high':16.5,'unit':''},
+            {'name':'Vtxmass','bins':50,'low':0,'high':100,'unit':'[GeV]'},
+            {'name':'Vtxpt','bins':50,'low':0,'high':200,'unit':'[GeV]'},
+            {'name':'Posip2dFrac','bins':10,'low':0,'high':1.001,'unit':''},
+            {'name':'NAvgMissHitsAfterVert','bins':12,'low':0,'high':6,'unit':''},
+            {'name':'glxyrmsclr','bins':50,'low':0,'high':2.5,'unit':''},
+            {'name':'bestclusterN','bins':15,'low':1.5,'high':16.5,'unit':''},
+	       ]
