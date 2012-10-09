4008b2ceeea1ec9eeb2c764fa72092382f850c6f
Andrzej Zuranski <zuranski@sesame2.(none)>
2012-10-09 07:40:47 -0400

    trigger filter for unprescaled triggers

--- steps/trigger.py
+++ steps/trigger.py
@@ -5,7 +5,6 @@ class hltFilter(analysisStep) :
 
     def __init__(self,hltPathName):
         self.hltPathName = hltPathName
-        self.moreName = self.hltPathName
 
     def select (self,e) :
         return e["triggered"][self.hltPathName]
@@ -14,10 +13,19 @@ class hltFilterWildcard(analysisStep) :
 
     def __init__(self,hltPathWildcard):
         self.hltPathWildcard = hltPathWildcard
-        self.moreName = hltPathWildcard+"*"
 
-    def select (self,e) :
-	for path in e["triggered"]:
-		if self.hltPathWildcard in path.first and path.second: return True
+    def select (self,e):
+        for path in e["triggered"]:
+            if self.hltPathWildcard in path.first and path.second: return True
         return False
 #####################################
+class hltFilterWildcardUnprescaled(analysisStep):
+
+    def __init__(self,hltPathWildcard):
+        self.hltPathWildcard = hltPathWildcard
+
+    def select (self,e):
+        for path,prescale in zip(e["triggered"],e["prescaled"]):
+			if self.hltPathWildcard in path.first and path.second and prescale.second==1: return True
+        return False
+    
