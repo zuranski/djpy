75eb3ad9f8320932a977477f76d2342b22b46b5a
Andrzej Zuranski <zuranski@sesame2.(none)>
2012-10-11 16:49:56 -0400

    small cleaning for TrueLxy

--- calculables/Matching.py
+++ calculables/Matching.py
@@ -3,11 +3,22 @@ from utils import MatchByDR
 
 class jetTrueLxy(wrappedChain.calculable):
 	def update(self,ignored):
-		if self.source['isRealData'] : self.value = None; return
-		if len(self.source['genjetPt']) == 0 : self.value = None; return
+		if self.source['isRealData'] or len(self.source['genjetPt']) == 0: 
+			self.value = [True for i in range(len(self.source['jetPt']))]
+			return
 		matches = MatchByDR(self.source['jetEta'],
 							self.source['jetPhi'],
 							self.source['genjetEta'],
 							self.source['genjetPhi'], 0.3)
-		self.value = [(self.source['genjetLxy'][matches[i]] if matches[i] is not None else -1)
+		self.value = [(self.source['genjetLxy'][matches[i]]/10. if matches[i] is not None else -1)
 					  for i in range(len(matches))]
+
+class dijetTrueLxy(wrappedChain.calculable):
+	def update(self,ignored):
+		if self.source['isRealData'] or len(self.source['genjetPt']) == 0: 
+			self.value = [True for i in range(len(self.source['dijetPt']))]
+			return
+		self.value = [self.source['jetTrueLxy'][self.source['dijetIdx1'][i]] 
+                      if self.source['jetTrueLxy'][self.source['dijetIdx1'][i]] ==
+                      self.source['jetTrueLxy'][self.source['dijetIdx2'][i]] != -1 else -1
+                      for i in range(len(self.source['dijetPt']))]
