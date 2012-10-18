5ee70aa50eab9c249f5ee34c4cf17cf2b7e86f21
Andrzej Zuranski <zuranski@sesame2.(none)>
2012-10-18 10:05:16 -0400

    single jet Promptness Var calculable.

--- calculables/Vars.py
+++ calculables/Vars.py
@@ -1,7 +1,7 @@
 from supy import wrappedChain
 from utils import DeltaR
 
-class DijetVar(wrappedChain.calculable):
+class DiJetVar(wrappedChain.calculable):
 	def __init__(self,indices=None):
 		self.indices = indices if indices is not None else 'dijetIndices'
 
@@ -10,28 +10,36 @@ class DijetVar(wrappedChain.calculable):
 		for idx in self.source[self.indices]:
 			self.value[idx] = self.calculate(idx)
 
-class dijetVtxNTotRatio(DijetVar):
-	def calculate(self,idx):
-		return self.source['dijetVtxN'][idx]/float(self.source['dijetNDispTracks'][idx]
-				+self.source['dijetNPromptTracks'][idx])
+class JetVar(wrappedChain.calculable):
+	def __init__(self,indices=None):
+		self.indices = indices if indices is not None else 'dijetIndices'
 
-class dijetVtxNRatio(DijetVar):
+	def update(self,ignored):
+		self.value = [-1 for i in range(len(self.source['jetPt']))]
+		for idx in self.source[self.indices]:
+			self.value[idx] = self.calculate(idx)
+
+class jetPromptness(JetVar):
+	def calculate(self,idx):
+		return self.source['jetNPromptTracks'][idx]*self.source['jetPromptEnergyFrac'][idx]
+
+class dijetVtxNRatio(DiJetVar):
 	def calculate(self,idx):
 		return self.source['dijetVtxN'][idx]/float(self.source['dijetNDispTracks'][idx]) if self.source['dijetNDispTracks'][idx] > 0 else -1
 
-class dijetVtxptRatio(DijetVar):
+class dijetVtxptRatio(DiJetVar):
 	def calculate(self,idx):
 		return self.source['dijetVtxpt'][idx]/float(self.source['dijetPt'][idx])
 
-class dijetPromptness(DijetVar):
+class dijetPromptness(DiJetVar):
 	def calculate(self,idx):
 		return self.source['dijetNPromptTracks'][idx]*self.source['dijetPromptEnergyFrac'][idx]
 
-class dijetPromptness1(DijetVar):
+class dijetPromptness1(DiJetVar):
 	def calculate(self,idx):
 		return self.source['jetNPromptTracks'][self.source['dijetIdx1'][idx]]*self.source['jetPromptEnergyFrac'][self.source['dijetIdx1'][idx]]
 
-class dijetPromptness2(DijetVar):
+class dijetPromptness2(DiJetVar):
 	def calculate(self,idx):
 		return self.source['jetNPromptTracks'][self.source['dijetIdx2'][idx]]*self.source['jetPromptEnergyFrac'][self.source['dijetIdx2'][idx]]
 
