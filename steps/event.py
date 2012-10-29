c1181b208a97d4b3567cb77018c3e22ccaf6fc87
Andrzej Zuranski <zuranski@sesame2.(none)>
2012-10-29 15:51:38 -0400

    offline PF HT

--- steps/event.py
+++ steps/event.py
@@ -2,6 +2,7 @@ from supy import analysisStep
 
 class general(analysisStep):
 	def uponAcceptance(self,e): 
+		self.book.fill(e['pfHT'],'pfHT',100,0,1e3,None,title = 'PfJets ; pfHT [GeV] ; events / bin')
 		self.book.fill(e['PfHt'],'PfHt',100,0,1e3,None,title = 'PfJets ; PfHt [GeV] ; events / bin')
 		self.book.fill(e['nPfJets'],'nPfJets',16,-0.5,15.5,None,title='nPfJets ; nPfJets ; events / bin')
 		self.book.fill(e['nPV'],'nPV',50,-0.5,49.5,None,title='nPV ; nPV ; events / bin')
