502303cd89f4ad044dac28c8e0b412d5adab5995
Andrzej Zuranski <zuranski@sesame2.(none)>
2012-11-07 11:14:27 -0500

    new trigger selector functions.

--- steps/trigger.py
+++ steps/trigger.py
@@ -11,14 +11,17 @@ class hltFilter(analysisStep) :
 #####################################
 class hltFilterWildcard(analysisStep) :
 
-    def __init__(self,hltPathWildcard):
+    def __init__(self,hltPathWildcard,veto=False):
         self.hltPathWildcard = hltPathWildcard
-        self.moreName = hltPathWildcard
+        self.veto = veto
+        self.moreName = hltPathWildcard+("* veto" if self.veto else "*")
 
     def select (self,e):
+        fired = False
         for path in e["triggered"]:
-            if self.hltPathWildcard in path.first and path.second: return True
-        return False
+            if self.hltPathWildcard in path.first and path.second: fired = True
+        if self.veto: return True if not fired else False
+        return True if fired else False
 #####################################
 class hltFilterWildcardUnprescaled(analysisStep):
 
@@ -41,4 +44,26 @@ class hltTriggerObjectMultiplicity(analysisStep):
 		for tag in e["trgobjTag"]:
 			if tag == self.objectName : nObjects += 1
 		if nObjects >= self.min: return True
+		return False
+#####################################
+class hltIsPresent(analysisStep):
+
+	def __init__(self,hltPathWildcard):
+		self.hltPathWildcard = hltPathWildcard
+		self.moreName = hltPathWildcard
+
+	def select (self,e):
+		for path in e["triggered"]:
+			if self.hltPathWildcard in path.first : return True
+		return False
+#####################################
+class hltIsPresentUnprescaled(analysisStep):
+
+	def __init__(self,hltPathWildcard):
+		self.hltPathWildcard = hltPathWildcard
+		self.moreName = hltPathWildcard
+
+	def select (self,e):
+		for path,prescale in zip(e["triggered"],e["prescaled"]):
+			if self.hltPathWildcard in path.first  and prescale.second==1: return True
 		return False
