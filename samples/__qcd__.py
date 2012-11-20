9a5338c36c4823c27219437bad974a0a54c6f0fd
Andrzej Zuranski <zuranski@sesame2.(none)>
2012-11-20 11:50:02 -0500

    updated ntuples location/naming

--- samples/__qcd__.py
+++ samples/__qcd__.py
@@ -16,11 +16,11 @@ qcd.add('qcd_600_800','%s/%s/%s")'%(pnfs,loc,"QCD_Pt_600to800") ,xs=15.6) #/pb
 '''
 
 #PU
-loc='/tigress-hsm/zuranski/work/cms/data/DJTuple44x_PROD/'
-qcd.add('qcd_50_80','%s'%(fileListFromDisk(loc+"QCD_Pt_50to80")),xs=6.36e6) #/pb
-qcd.add('qcd_80_120','%s'%(fileListFromDisk(loc+"QCD_Pt_80to120")),xs=7.843e5) #/pb
-qcd.add('qcd_120_170','%s'%(fileListFromDisk(loc+"QCD_Pt_120to170")),xs=11.51e4) #/pb
-qcd.add('qcd_170_300','%s'%(fileListFromDisk(loc+"QCD_Pt_170to300")),xs=2.426e4) #/pb
-qcd.add('qcd_300_470','%s'%(fileListFromDisk(loc+"QCD_Pt_300to470")),xs=11.68e2) #/pb
-qcd.add('qcd_470_600','%s'%(fileListFromDisk(loc+"QCD_Pt_470to600")),xs=70.22) #/pb
-qcd.add('qcd_600_800','%s'%(fileListFromDisk(loc+"QCD_Pt_600to800")),xs=15.55) #/pb
+loc='/tigress-hsm/zuranski/work/cms/data/DJTuple53x_PROD/'
+qcd.add('qcd_50_80','%s'%(fileListFromDisk(loc+"QCD_Pt_50to80")),xs=8.148778e6) #/pb
+qcd.add('qcd_80_120','%s'%(fileListFromDisk(loc+"QCD_Pt_80to120")),xs=10.3368e5) #/pb
+qcd.add('qcd_120_170','%s'%(fileListFromDisk(loc+"QCD_Pt_120to170")),xs=15.62933e4) #/pb
+qcd.add('qcd_170_300','%s'%(fileListFromDisk(loc+"QCD_Pt_170to300")),xs=3.413815e4) #/pb
+qcd.add('qcd_300_470','%s'%(fileListFromDisk(loc+"QCD_Pt_300to470")),xs=17.59549e2) #/pb
+qcd.add('qcd_470_600','%s'%(fileListFromDisk(loc+"QCD_Pt_470to600")),xs=113.8791) #/pb
+qcd.add('qcd_600_800','%s'%(fileListFromDisk(loc+"QCD_Pt_600to800")),xs=26.9921) #/pb
--- samples/__sigmc__.py
+++ samples/__sigmc__.py
@@ -25,19 +25,11 @@ sigmc.add('H_200_X_50b','%s/%s/%s")'%(pnfs,loc,"MH_200_MX_50_Ctau_200b/") ,xs=50
 '''
 
 # PU
-loc='/tigress-hsm/zuranski/work/cms/data/DJTuple44x_PROD/'
+loc='/tigress-hsm/zuranski/work/cms/data/DJTuple53x_PROD/'
 # u-d-s quark samples
-sigmc.add('Huds_1000_X_350','%s'%(fileListFromDisk(loc+"MH_1000_MX_350_Ctau_350/")) ,xs=500.1) #/pb
-sigmc.add('Huds_1000_X_150','%s'%(fileListFromDisk(loc+"MH_1000_MX_150_Ctau_100/")) ,xs=500.1) #/pb
-sigmc.add('Huds_1000_X_50','%s'%(fileListFromDisk(loc+"MH_1000_MX_50_Ctau_40/")) ,xs=500.1) #/pb
-sigmc.add('Huds_400_X_150','%s'%(fileListFromDisk(loc+"MH_400_MX_150_Ctau_400/")) ,xs=500.1) #/pb
-sigmc.add('Huds_400_X_50','%s'%(fileListFromDisk(loc+"MH_400_MX_50_Ctau_80/")) ,xs=500.1) #/pb
-sigmc.add('Huds_200_X_50','%s'%(fileListFromDisk(loc+"MH_200_MX_50_Ctau_200/")) ,xs=500.1) #/pb
-
-# b quark samples
-sigmc.add('Hb_1000_X_350','%s'%(fileListFromDisk(loc+"MH_1000_MX_350_Ctau_350b/")) ,xs=500.1) #/pb
-sigmc.add('Hb_1000_X_150','%s'%(fileListFromDisk(loc+"MH_1000_MX_150_Ctau_100b/")) ,xs=500.1) #/pb
-sigmc.add('Hb_1000_X_50','%s'%(fileListFromDisk(loc+"MH_1000_MX_50_Ctau_40b/")) ,xs=500.1) #/pb
-sigmc.add('Hb_400_X_150','%s'%(fileListFromDisk(loc+"MH_400_MX_150_Ctau_400b/")) ,xs=500.1) #/pb
-sigmc.add('Hb_400_X_50','%s'%(fileListFromDisk(loc+"MH_400_MX_50_Ctau_80b/")) ,xs=500.1) #/pb
-sigmc.add('Hb_200_X_50','%s'%(fileListFromDisk(loc+"MH_200_MX_50_Ctau_200b/")) ,xs=500.1) #/pb
+sigmc.add('H_1000_X_350','%s'%(fileListFromDisk(loc+"MH_1000_MX_350_Ctau_35To3500/")) ,xs=500.1) #/pb
+sigmc.add('H_1000_X_150','%s'%(fileListFromDisk(loc+"MH_1000_MX_150_Ctau_10To1000/")) ,xs=500.1) #/pb
+sigmc.add('H_1000_X_50','%s'%(fileListFromDisk(loc+"MH_1000_MX_50_Ctau_4To400/")) ,xs=500.1) #/pb
+sigmc.add('H_400_X_150','%s'%(fileListFromDisk(loc+"MH_400_MX_150_Ctau_40To4000/")) ,xs=500.1) #/pb
+sigmc.add('H_400_X_50','%s'%(fileListFromDisk(loc+"MH_400_MX_50_Ctau_8To800/")) ,xs=500.1) #/pb
+sigmc.add('H_200_X_50','%s'%(fileListFromDisk(loc+"MH_200_MX_50_Ctau_20To2000/")) ,xs=500.1) #/pb
