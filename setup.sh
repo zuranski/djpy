f4ff8285f7fe1061a13e04b995a03ce22506d7c5
Andrzej Zuranski <zuranski@sesame2.(none)>
2012-12-03 14:25:31 -0500

    setup for local running at Princeton.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
+
+export PATH=/home/zuranski/soft/Python-2.7.3/bin:$PATH
+export LD_LIBRARY_PATH=/home/zuranski/soft/Python-2.7.3/lib:$LD_LIBRARY_PATH
+. /home/zuranski/soft/root/bin/thisroot.sh
+
 export PYTHONPATH=$PYTHONPATH:`pwd`
 export PATH=$PATH:`pwd`/supy/bin
