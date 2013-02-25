60ea2caf60a7b1012020b06bcb07c9b48ca357d9
Andrzej Zuranski <zuranski@sesame2.(none)>
2013-02-25 08:42:04 -0500

    new ntuples

--- samples/__qcd__.py
+++ samples/__qcd__.py
@@ -17,6 +17,7 @@ qcd.add('qcd_600_800','%s/%s/%s")'%(pnfs,loc,"QCD_Pt_600to800") ,xs=15.6) #/pb
 
 #PU
 loc='/tigress-hsm/zuranski/work/cms/data/DJTuple53x_DEV/'
+
 qcd.add('qcd_50_80','%s'%(fileListFromDisk(loc+"QCD_Pt_50to80")),xs=8.148778e6) #/pb
 qcd.add('qcd_80_120','%s'%(fileListFromDisk(loc+"QCD_Pt_80to120")),xs=10.3368e5) #/pb
 qcd.add('qcd_120_170','%s'%(fileListFromDisk(loc+"QCD_Pt_120to170")),xs=15.62933e4) #/pb
@@ -24,3 +25,13 @@ qcd.add('qcd_170_300','%s'%(fileListFromDisk(loc+"QCD_Pt_170to300")),xs=3.413815
 qcd.add('qcd_300_470','%s'%(fileListFromDisk(loc+"QCD_Pt_300to470")),xs=17.59549e2) #/pb
 qcd.add('qcd_470_600','%s'%(fileListFromDisk(loc+"QCD_Pt_470to600")),xs=113.8791) #/pb
 qcd.add('qcd_600_800','%s'%(fileListFromDisk(loc+"QCD_Pt_600to800")),xs=26.9921) #/pb
+
+'''
+qcd.add('qcd_50_80','%s'%(fileListFromDisk(loc+"QCD_Pt_50to80")),lumi=1) #/pb
+qcd.add('qcd_80_120','%s'%(fileListFromDisk(loc+"QCD_Pt_80to120")),lumi=1) #/pb
+qcd.add('qcd_120_170','%s'%(fileListFromDisk(loc+"QCD_Pt_120to170")),lumi=1) #/pb
+qcd.add('qcd_170_300','%s'%(fileListFromDisk(loc+"QCD_Pt_170to300")),lumi=1) #/pb
+qcd.add('qcd_300_470','%s'%(fileListFromDisk(loc+"QCD_Pt_300to470")),lumi=1) #/pb
+qcd.add('qcd_470_600','%s'%(fileListFromDisk(loc+"QCD_Pt_470to600")),lumi=1) #/pb
+qcd.add('qcd_600_800','%s'%(fileListFromDisk(loc+"QCD_Pt_600to800")),lumi=1) #/pb
+'''
