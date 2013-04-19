6f8999d9e5985eed9baf977a86ccb1d01dd27116
Andrzej Zuranski <zuranski@della3.(none)>
2013-04-19 18:56:50 -0400

    jet mering plots fixed

--- steps/genjets.py
+++ steps/genjets.py
@@ -11,17 +11,19 @@ class general(analysisStep):
 		if e['genqLxy'][2]<50:
 			self.book.fill(n2,'n',4,-0.5,3.5,None,title='genJets ; n ; events/bin') 
 
+		XtoJets=[6001114,6002114,6003114]
+
 		if (e['genqLxy'][0]<50 
 		and e['genqPt'][0]>40 
 	    and e['genqPt'][1]>40 
-        and abs(e['genqEta'][0])<2 
+        and abs(e['genqEta'][0])<2
         and abs(e['genqEta'][1])<2
-		and e['genHT']>200):
+		and e['XpdgId'][0] in XtoJets):
 			self.book.fill(len(e['genjetLxy1']),'nnmerging',4,-0.5,3.5,None,title='genJets ; n ; events/bin') 
 		if (e['genqLxy'][2]<50 
         and e['genqPt'][2]>40
         and e['genqPt'][3]>40 
         and abs(e['genqEta'][2])<2 
         and abs(e['genqEta'][3])<2
-		and e['genHT']>200):
+		and e['XpdgId'][1] in XtoJets):
 			self.book.fill(len(e['genjetLxy2']),'nnmerging',4,-0.5,3.5,None,title='genJets ; n ; events/bin') 
