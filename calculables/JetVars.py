a524c376a21ba55e8670a97b96cf3d65c32b1a10
Andrzej Zuranski <zuranski@della3.(none)>
2013-05-20 16:28:29 -0400

    reorganized

--- calculables/JetVars.py
+++ calculables/JetVars.py
@@ -1,5 +1,5 @@
 from supy import wrappedChain
-from utils import DeltaR,p
+from utils import DeltaR,p,MatchByDR
 
 class jetVar(wrappedChain.calculable):
 	fixes=('jet','')
@@ -32,14 +32,6 @@ class PtGeom(jetVar):
 	def calculate(self,idx):
 		return self.source['jetPt'][idx]*self.source['jetgenjetCorr'][idx]
 
-class ksP(wrappedChain.calculable):
-	def update(self,ignored):
-		self.value = [p(self.source['ksPt'][idx],self.source['ksEta'][idx]) for idx in self.source['ksIndices']]
-
-class genjetPtDiff(jetVar):
-	def calculate(self,idx):
-		return (self.source['jetPt'][idx]-self.source['jetgenjetPt'][idx])/self.source['jetgenjetPt'][idx]
-
 class genjetDR(jetVar):
 	def calculate(self,idx):
 		return DeltaR(
@@ -48,3 +40,55 @@ class genjetDR(jetVar):
 				      self.source['jetgenjetEta'][idx],
 				      self.source['jetgenjetPhi'][idx]
 			         ) 
+
+class genjetPtDiff(jetVar):
+	def calculate(self,idx):
+		return (self.source['jetPt'][idx]-self.source['jetgenjetPt'][idx])/self.source['jetgenjetPt'][idx]
+
+class jetgenjetMatch(wrappedChain.calculable):
+	def update(self,ignored):
+		if self.source['realData'] or len(self.source['XpdgId']) == 0: 
+			self.value = [True for i in self.source['jetIndices']]
+			return
+		self.value = MatchByDR(self.source['jetEta'],
+							self.source['jetPhi'],
+							self.source['genjetEta'],
+							self.source['genjetPhi'],0.15) 
+
+class jetgenjet(wrappedChain.calculable):
+	fixes=('jetgenjet','')
+	def update(self,ignored):
+		self.value = [self.source['genjet'+self.var][self.source['jetgenjetMatch'][i]] 
+					  if self.source['jetgenjetMatch'][i] is not None else -999 for i in self.source['jetIndices']]
+
+class Eta(jetgenjet): var='Eta'
+class Phi(jetgenjet): var='Phi'
+class Pt(jetgenjet): var='Pt'
+class Energy(jetgenjet):	var='Energy'
+class Lxy(jetgenjet): var='Lxy'
+class Angle(jetgenjet): var='Angle'
+class Corr(jetgenjet): var='Corr'
+class DeltaR(jetgenjet): var='DeltaR'
+
+# very special case
+class N(jetgenjet):
+	def update(self,ignored):
+		self.value=[None for i in self.source['jetIndices']]
+
+		belongsTo=[None for i in self.source['jetIndices']]
+		for i in self.source['jetIndices']:
+			# check if the jet is matched
+			if self.source['jetgenjetMatch'][i] is None : continue
+			# check from which X particle it comes [1]
+			genEta=self.source['genjetEta'][self.source['jetgenjetMatch'][i]]
+			belongsTo[i]=0 if genEta in self.source['genjetEta1'] else 1
+
+		# count nGenJets for exo 1 and 2
+		nGen1=sum([1 for pt in self.source['genjetPt1'] if pt>40 ]) 
+		nGen2=sum([1 for pt in self.source['genjetPt2'] if pt>40 ]) 
+		nGen=[nGen1,nGen2]
+		# count nRecoJet matched for exo 1 and 2
+		nReco=[belongsTo.count(0),belongsTo.count(1)]
+		# if the two counts don't agree per X throw out
+		for i,x in enumerate(belongsTo):
+			if x is not None and nReco[x]==nGen[x]: self.value[i]=nReco[x]
