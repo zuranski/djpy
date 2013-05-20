d9fefad090344bad219b6e5ade4447952f5d0c51
Andrzej Zuranski <zuranski@della3.(none)>
2013-05-20 16:49:50 -0400

    added labels to plots

--- steps/plots.py
+++ steps/plots.py
@@ -15,8 +15,8 @@ class plots(analysisStep):
 			for var in self.vars:
 				for idx in e[self.indices]:
 					self.book.fill(e[self.prefix+var['name']][idx],var['name']+'_h_'+self.tag,var['bins'],var['low'],
-								   var['high'],w=None,title="%s; %s%s ; %s / bin "
-								   %(self.prefix,var['name'],var['unit'],self.prefix))
+								   var['high'],w=None,title="%s; %s ; %s / bin "
+								   %(self.prefix,var['label'],self.prefix))
 
 		if self.plot2D:
 			for var1,var2 in itertools.combinations(self.vars,2):
@@ -24,8 +24,8 @@ class plots(analysisStep):
 					self.book.fill((e[self.prefix+var1['name']][idx],e[self.prefix+var2['name']][idx]),
                                    var1['name']+'_'+var2['name']+'_h_'+self.tag,(var1['bins'],var2['bins']),
                                    (var1['low'],var2['low']),(var1['high'],var2['high']),
-								   w=None,title="%s ; %s%s; %s%s" 
-								   %(self.prefix,var1['name'],var1['unit'],var2['name'],var2['unit']))
+								   w=None,title="%s ; %s; %s" 
+								   %(self.prefix,var1['label'],var2['label']))
 
 class ABCDplots(analysisStep):
 	def __init__(self,indices=None):
@@ -49,121 +49,122 @@ class ABCDEFGHplots(analysisStep):
 
 class general(plots):
 	vars = [
-            {'name':'Pt','bins':50,'low':40,'high':300,'unit':'[GeV]'},
-            {'name':'Eta','bins':50,'low':-3,'high':3,'unit':''},
-            {'name':'Phi','bins':50,'low':-3.5,'high':3.5,'unit':''},
-            {'name':'Energy','bins':50,'low':40,'high':500,'unit':'[GeV]'},
-            {'name':'Mass','bins':100,'low':0,'high':500,'unit':'[GeV]'},
-            {'name':'NConstituents','bins':100,'low':0.5,'high':100.5,'unit':''},
+            {'name':'Pt','bins':50,'low':40,'high':300,'label':'p_{T} [GeV]'},
+            {'name':'Eta','bins':50,'low':-3,'high':3,'label':'#eta'},
+            {'name':'Phi','bins':50,'low':-3.5,'high':3.5,'label':'#phi'},
+            {'name':'Energy','bins':50,'low':40,'high':500,'label':'Energy [GeV]'},
+            {'name':'Mass','bins':100,'low':0,'high':500,'label':'mass [GeV]'},
+            {'name':'NConstituents','bins':100,'low':0.5,'high':100.5,'label':'N constituents'},
            ]
 
 class fractions(plots):
 	vars = [
-            {'name':'NeuHadFrac','bins':50,'low':0,'high':1,'unit':''},
-            {'name':'NeuHadN','bins':15,'low':-0.5,'high':29.5,'unit':''},
-            {'name':'ChgHadFrac','bins':50,'low':0,'high':1,'unit':''},
-            {'name':'ChgHadN','bins':50,'low':-0.5,'high':99.5,'unit':''},
-            {'name':'MuFrac','bins':50,'low':0,'high':1,'unit':''},
-            {'name':'MuN','bins':10,'low':-0.5,'high':9.5,'unit':''},
-            {'name':'EleFrac','bins':50,'low':0,'high':1,'unit':''},
-            {'name':'EleN','bins':10,'low':-0.5,'high':9.5,'unit':''},
-            {'name':'PhFrac','bins':50,'low':0,'high':1,'unit':''},
-            {'name':'PhN','bins':50,'low':-0.5,'high':99.5,'unit':''},
+            {'name':'NeuHadFrac','bins':50,'low':0,'high':1,'label':'neutral Had Fraction'},
+            {'name':'NeuHadN','bins':15,'low':-0.5,'high':29.5,'label':'neutral Had Multiplicity'},
+            {'name':'ChgHadFrac','bins':50,'low':0,'high':1,'label':'charged Had Fraction'},
+            {'name':'ChgHadN','bins':50,'low':-0.5,'high':99.5,'label':'charged Had Multiplicity'},
+            {'name':'MuFrac','bins':50,'low':0,'high':1,'label':'muon Fraction'},
+            {'name':'MuN','bins':10,'low':-0.5,'high':9.5,'label':'muon Multplicity'},
+            {'name':'EleFrac','bins':50,'low':0,'high':1,'label':'electron Fraction'},
+            {'name':'EleN','bins':10,'low':-0.5,'high':9.5,'label':'electron Multplicity'},
+            {'name':'PhFrac','bins':50,'low':0,'high':1,'label':'photon Fraction'},
+            {'name':'PhN','bins':50,'low':-0.5,'high':99.5,'label':'photon Multiplicity'},
            ]
 
 class promptness(plots):
 	vars = [
-            {'name':'NPromptTracks','bins':30,'low':-0.5,'high':29.5,'unit':''},
-            {'name':'NDispTracks','bins':30,'low':-0.5,'high':29.5,'unit':''},
-            {'name':'PromptEnergyFrac','bins':50,'low':0,'high':1,'unit':''},
+            {'name':'NPromptTracks','bins':30,'low':-0.5,'high':29.5,'label':'N prompt tracks'},
+            {'name':'NDispTracks','bins':30,'low':-0.5,'high':29.5,'label':'N displaced tracks'},
+            {'name':'PromptEnergyFrac','bins':50,'low':0,'high':1,'label':'Prompt Energy Fraction'},
            ]
 
 class vertices(plots):
 	vars = [
-            {'name':'Lxy','bins':50,'low':0,'high':60,'unit':'[cm]'},
-            {'name':'Lxysig','bins':50,'low':0,'high':100,'unit':''},
-            {'name':'VtxN','bins':15,'low':1.5,'high':16.5,'unit':''},
-            {'name':'Vtxmass','bins':50,'low':0,'high':100,'unit':'[GeV]'},
-            {'name':'Vtxpt','bins':50,'low':0,'high':200,'unit':'[GeV]'},
-            {'name':'VtxNRatio','bins':20,'low':0,'high':1,'unit':''},
-            {'name':'Posip2dFrac','bins':10,'low':0,'high':1.001,'unit':''},
-            {'name':'NAvgMissHitsAfterVert','bins':6,'low':0,'high':6,'unit':''},
+            {'name':'Lxy','bins':50,'low':0,'high':60,'label':'L_{xy} [cm]'},
+            {'name':'Lxysig','bins':50,'low':0,'high':100,'label':'L_{xy} significance'},
+            {'name':'VtxN','bins':15,'low':1.5,'high':16.5,'label':'Vtx multplicity'},
+            {'name':'Vtxmass','bins':50,'low':0,'high':100,'label':'Vtx mass [GeV]'},
+            {'name':'Vtxpt','bins':50,'low':0,'high':200,'label':'Vtx p_{T} [GeV]'},
+            {'name':'VtxNRatio','bins':20,'low':0,'high':1,'label':'Vtx N ratio'},
+            {'name':'Posip2dFrac','bins':10,'low':0,'high':1.001,'label':'Tracks with positive IP Fraction'},
+            {'name':'NAvgMissHitsAfterVert','bins':6,'low':0,'high':6,'label':'Missing Hits per track afert Vertex'},
            ]
 
 class clusters(plots):
 	vars = [
-            {'name':'glxyrmsclr','bins':50,'low':0,'high':2.5,'unit':''},
-            {'name':'bestclusterN','bins':15,'low':1.5,'high':16.5,'unit':''},
+            {'name':'glxyrmsclr','bins':50,'low':0,'high':2.5,'label':'Cluster RMS'},
+            {'name':'bestclusterN','bins':15,'low':1.5,'high':16.5,'label':'Cluster Track Multplicity'},
            ]
 
 class ABCDvars(plots):
 	vars = [
-            {'name':'Discriminant','bins':14,'low':0.,'high':1.,'unit':''},
-            {'name':'PromptEnergyFrac1','bins':50,'low':0.,'high':1,'unit':''},
-            {'name':'PromptEnergyFrac2','bins':50,'low':0.,'high':1,'unit':''},
-            {'name':'NPromptTracks1','bins':11,'low':-0.5,'high':10.5,'unit':''},
-            {'name':'NPromptTracks2','bins':11,'low':-0.5,'high':10.5,'unit':''},
-            {'name':'Mass','bins':20,'low':0,'high':800,'unit':'[GeV]'},
+            {'name':'Discriminant','bins':14,'low':0.,'high':1.,'label':'Vtx/Cluster Discriminant'},
+            {'name':'PromptEnergyFrac1','bins':50,'low':0.,'high':1,'label':'Prompt Energy Fraction 1'},
+            {'name':'PromptEnergyFrac2','bins':50,'low':0.,'high':1,'label':'Prompt Energy Fracion 2'},
+            {'name':'NPromptTracks1','bins':11,'low':-0.5,'high':10.5,'label':'N Prompt Tracks 1'},
+            {'name':'NPromptTracks2','bins':11,'low':-0.5,'high':10.5,'label':'N Prompt Tracks 2'},
+            {'name':'Mass','bins':20,'low':0,'high':800,'label':'Dijet Mass [GeV]'},
            ]
 
 class cutvars(plots):
 	vars = [
-            {'name':'Lxy','bins':50,'low':0,'high':60,'unit':'[cm]'},
-            {'name':'PromptEnergyFrac1','bins':50,'low':0.,'high':1,'unit':''},
-            {'name':'PromptEnergyFrac2','bins':50,'low':0.,'high':1,'unit':''},
-            {'name':'NPromptTracks1','bins':11,'low':-0.5,'high':10.5,'unit':''},
-            {'name':'NPromptTracks2','bins':11,'low':-0.5,'high':10.5,'unit':''},
-            {'name':'Lxysig','bins':50,'low':0,'high':100,'unit':''},
-            {'name':'VtxN','bins':15,'low':1.5,'high':16.5,'unit':''},
-            {'name':'Vtxmass','bins':50,'low':0,'high':100,'unit':'[GeV]'},
-            {'name':'Vtxpt','bins':50,'low':0,'high':200,'unit':'[GeV]'},
-            {'name':'Posip2dFrac','bins':10,'low':0,'high':1.001,'unit':''},
-            {'name':'NAvgMissHitsAfterVert','bins':12,'low':0,'high':6,'unit':''},
-            {'name':'glxyrmsclr','bins':50,'low':0,'high':2.5,'unit':''},
-            {'name':'bestclusterN','bins':15,'low':1.5,'high':16.5,'unit':''},
+            {'name':'Lxy','bins':50,'low':0,'high':60,'label':'L_{xy} [cm]'},
+            {'name':'PromptEnergyFrac1','bins':20,'low':0.,'high':1,'label':'Prompt Energy Fraction 1'},
+            {'name':'PromptEnergyFrac2','bins':20,'low':0.,'high':1,'label':'Prompt Energy Fracion 2'},
+            {'name':'NPromptTracks1','bins':11,'low':-0.5,'high':10.5,'label':'N Prompt Tracks 1'},
+            {'name':'NPromptTracks2','bins':11,'low':-0.5,'high':10.5,'label':'N Prompt Tracks 2'},
+            {'name':'Lxysig','bins':25,'low':0,'high':100,'label':'L_{xy} significance'},
+            {'name':'VtxN','bins':15,'low':1.5,'high':16.5,'label':'Vtx Track Multiplicity'},
+            {'name':'Vtxmass','bins':25,'low':0,'high':100,'label':'Vtx mass [GeV]'},
+            {'name':'Vtxpt','bins':25,'low':0,'high':200,'label':'Vtx p_{T} [GeV]'},
+            {'name':'Posip2dFrac','bins':10,'low':0,'high':1.001,'label':'Tracks with positive IP Fraction'},
+            {'name':'NAvgMissHitsAfterVert','bins':12,'low':0,'high':6,'label':'Missing Hits per track after Vertex'},
+            {'name':'glxyrmsclr','bins':10,'low':0,'high':1,'label':'Cluster RMS'},
+            {'name':'bestclusterN','bins':15,'low':1.5,'high':16.5,'label':'Cluster Tracks Multiplicity'},
 	       ]
 
 class observables(plots):
 	vars = [
-            {'name':'Mass','bins':100,'low':0,'high':500,'unit':'[GeV]'},
-            {'name':'Lxy','bins':50,'low':0,'high':60,'unit':'[GeV]'},
-            #{'name':'TrueLxy','bins':50,'low':0,'high':60,'unit':'[GeV]'},
+            {'name':'Mass','bins':50,'low':0,'high':500,'label':'Dijet Invariant Mass [GeV]'},
+            {'name':'Lxy','bins':50,'low':0,'high':60,'label':'L_{xy}[GeV]'},
 	       ]
 
 class trigvars(plots):
 	vars = [
-            {'name':'Pt','bins':10,'low':60,'high':300,'unit':'[GeV]'},
-            {'name':'Eta','bins':10,'low':-2,'high':2,'unit':''},
-            {'name':'Phi','bins':10,'low':-3.15,'high':3.15,'unit':''},
-            {'name':'NPromptTracks','bins':11,'low':-0.5,'high':10.5,'unit':''},
-            {'name':'PromptEnergyFrac','bins':10,'low':0.,'high':0.5,'unit':''},
+            {'name':'Pt','bins':10,'low':60,'high':300,'label':'p_{T} [GeV]'},
+            {'name':'Eta','bins':10,'low':-2,'high':2,'label':'#eta'},
+            {'name':'Phi','bins':10,'low':-3.15,'high':3.15,'label':'#phi'},
+            {'name':'NPromptTracks','bins':11,'low':-0.5,'high':10.5,'label':'N Prompt Tracks'},
+            {'name':'PromptEnergyFrac','bins':10,'low':0.,'high':0.5,'label':'Prompt Energy Fraction'},
            ]
 
 class kshort(plots):
 	vars = [
-            {'name':'P','bins':50,'low':0,'high':100,'unit':'[GeV]'},
-            {'name':'Pt','bins':50,'low':0,'high':40,'unit':'[GeV]'},
-            {'name':'Eta','bins':50,'low':-2,'high':2,'unit':''},
-            {'name':'Phi','bins':50,'low':-3.15,'high':3.15,'unit':''},
-            {'name':'Mass','bins':100,'low':0.43,'high':0.57,'unit':'[GeV]'},
-            {'name':'Ctau','bins':100,'low':0.,'high':12,'unit':'cm'},
-            {'name':'Lxy','bins':100,'low':0.,'high':60,'unit':'cm'},
-            {'name':'Lxyz','bins':100,'low':0.,'high':120,'unit':'cm'},
-            {'name':'JetPt','bins':100,'low':20.,'high':300,'unit':'[GeV]'},
-            {'name':'Trk1IP2d','bins':50,'low':-2.,'high':20,'unit':''},
-            {'name':'Trk2IP2d','bins':50,'low':-2.,'high':20,'unit':''},
-            {'name':'Trk1IP3d','bins':50,'low':0.,'high':25,'unit':''},
-            {'name':'Trk2IP3d','bins':50,'low':0.,'high':25,'unit':''},
+            {'name':'P','bins':50,'low':0,'high':100,'label':'momentum [GeV]'},
+            {'name':'Pt','bins':50,'low':0,'high':40,'label':'p_{T} [GeV]'},
+            {'name':'Eta','bins':50,'low':-2,'high':2,'label':'#eta'},
+            {'name':'Phi','bins':50,'low':-3.15,'high':3.15,'label':'#phi'},
+            {'name':'Mass','bins':100,'low':0.43,'high':0.57,'label':'mass [GeV]'},
+            {'name':'Ctau','bins':100,'low':0.,'high':12,'label':'c#tau [cm]'},
+            {'name':'Lxy','bins':100,'low':0.,'high':60,'label':'L_{xy} [cm]'},
+            {'name':'Lxyz','bins':100,'low':0.,'high':120,'label':'L_{xyz} [cm]'},
+            {'name':'JetPt','bins':100,'low':20.,'high':300,'label':'Jet p_{T} [GeV]'},
+            {'name':'Trk1IP2d','bins':50,'low':-2.,'high':20,'label':'IP1_{xy} [cm]'},
+            {'name':'Trk2IP2d','bins':50,'low':-2.,'high':20,'label':'IP2_{xy} [cm]'},
+            {'name':'Trk1IP3d','bins':50,'low':0.,'high':25,'label':'IP1_{xyz} [cm]'},
+            {'name':'Trk2IP3d','bins':50,'low':0.,'high':25,'label':'IP2_{xyz} [cm]'},
            ]
 
 class genjets(plots):
 	vars = [
-            {'name':'Pt','bins':50,'low':0,'high':500,'unit':'[GeV]'},
-            {'name':'Eta','bins':20,'low':-2,'high':2,'unit':''},
-            {'name':'Phi','bins':20,'low':-3.15,'high':3.15,'unit':''},
-            {'name':'genjetPtDiff','bins':50,'low':-1.,'high':1,'unit':''},
-            #{'name':'genjetEtaDiff','bins':50,'low':-.2,'high':.2,'unit':''},
-            #{'name':'genjetPhiDiff','bins':50,'low':-.2,'high':.2,'unit':''},
-            #{'name':'genjetPt','bins':100,'low':0.,'high':500,'unit':''},
-            {'name':'genjetLxy','bins':25,'low':0.,'high':50,'unit':''},
+            {'name':'Energy','bins':22,'low':60,'high':500,'label':'Energy [GeV]'},
+            {'name':'Pt','bins':22,'low':60,'high':500,'label':'p_{T} [GeV]'},
+            {'name':'Eta','bins':10,'low':-2,'high':2,'label':'#eta'},
+            {'name':'Phi','bins':10,'low':-3.15,'high':3.15,'label':'#phi'},
+            {'name':'genjetPtDiff','bins':50,'low':-1.,'high':1,'label':'(jet p_{T} - true p_{T} )/ true p_{T}'},
+            {'name':'genjetLxy','bins':30,'low':0.,'high':60,'label':'L_{xy} [cm]'},
+            {'name':'genjetAngle','bins':36,'low':0.,'high':12,'label':'approach Angle [deg]'},
+            {'name':'genjetDeltaR','bins':30,'low':0.,'high':3.15,'label':'q#bar{q} #Delta R'},
+            {'name':'NeuFrac','bins':15,'low':0.1,'high':1,'label':'Neutral Energy Fraction'},
+            {'name':'NDispTracks','bins':15,'low':-0.5,'high':29.5,'label':'N tracks'},
 		   ]
