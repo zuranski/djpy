1c8732c8815a092c6d87f177b6a8bd154e86f51d
Andrzej Zuranski <zuranski@della3.(none)>
2013-05-20 10:41:00 -0400

    some clean-up

---
+++ calculables/GenQuarks.py
@@ -0,0 +1,50 @@
+from supy import wrappedChain
+from utils import DeltaR
+
+class gendijet(wrappedChain.calculable):
+	def update(self,ignored):
+		self.value=[]
+		lxy=self.source['genqLxy']
+		N=len(self.source['genqLxy'])
+		for i in range(N-1):
+			for j in range(i+1,N):
+				if lxy[i]==lxy[j] : self.value.append((i,j))
+
+class gendijetIndices(wrappedChain.calculable):
+	def update(self,ignored):
+		self.value = [i for i in range(len(self.source['gendijet']))]
+
+class gendijetLxy(wrappedChain.calculable):
+	def update(self,ignored):
+		self.value = [self.source['genqLxy'][pair[0]] for pair in self.source['gendijet']]
+
+class gendijetCtau(wrappedChain.calculable):
+	def update(self,ignored):
+		self.value = [self.source['genqCtau'][pair[0]] for pair in self.source['gendijet']]
+
+class gendijetPt1(wrappedChain.calculable):
+	def update(self,ignored):
+		self.value = [self.source['genqPt'][pair[0]] for pair in self.source['gendijet']]
+
+class gendijetPt2(wrappedChain.calculable):
+	def update(self,ignored):
+		self.value = [self.source['genqPt'][pair[1]] for pair in self.source['gendijet']]
+
+class gendijetEta1(wrappedChain.calculable):
+	def update(self,ignored):
+		self.value = [abs(self.source['genqEta'][pair[0]]) for pair in self.source['gendijet']]
+
+class gendijetEta2(wrappedChain.calculable):
+	def update(self,ignored):
+		self.value = [self.source['genqEta'][pair[1]] for pair in self.source['gendijet']]
+
+class gendijetqID(wrappedChain.calculable):
+	def update(self,ignored):
+		self.value = [self.source['genqFlavor'][pair[0]] for pair in self.source['gendijet']]
+
+class gendijetDR(wrappedChain.calculable):
+	def update(self,ignored):
+		self.value = [DeltaR(self.source['genqEta'][pair[0]],
+							 self.source['genqPhi'][pair[0]],
+							 self.source['genqEta'][pair[1]],
+							 self.source['genqPhi'][pair[1]]) for pair in self.source['gendijet']]
--- calculables/__init__.py
+++ calculables/__init__.py
@@ -1,1 +1,1 @@
-import event,Indices,Matching,Vars,Overlaps,Muons,Acceptance,GenJets
+import event,Indices,Matching,Vars,Overlaps,Muons,GenQuarks,GenJets
