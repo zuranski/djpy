816451a982c89a24ce9013230bf17b329c20d9f6
Andrzej Zuranski <zuranski@della3.(none)>
2013-06-20 05:37:48 -0400

    removed scale factors

--- samples/__qcd__.py
+++ samples/__qcd__.py
@@ -18,15 +18,13 @@ qcd.add('qcd_600_800','%s/%s/%s")'%(pnfs,loc,"QCD_Pt_600to800") ,xs=15.6) #/pb
 #PU
 loc='/scratch/gpfs/zuranski/data/DJTuple53x_PROD/'
 
-scale=1.495
-
-qcd.add('qcd_50_80','%s'%(fileListFromDisk(loc+"QCD_Pt_50to80")),xs=8.148778e6/scale) #/pb
-qcd.add('qcd_80_120','%s'%(fileListFromDisk(loc+"QCD_Pt_80to120")),xs=10.3368e5/scale) #/pb
-qcd.add('qcd_120_170','%s'%(fileListFromDisk(loc+"QCD_Pt_120to170")),xs=15.62933e4/scale) #/pb
-qcd.add('qcd_170_300','%s'%(fileListFromDisk(loc+"QCD_Pt_170to300")),xs=3.413815e4/scale) #/pb
-qcd.add('qcd_300_470','%s'%(fileListFromDisk(loc+"QCD_Pt_300to470")),xs=17.59549e2/scale) #/pb
-qcd.add('qcd_470_600','%s'%(fileListFromDisk(loc+"QCD_Pt_470to600")),xs=113.8791/scale) #/pb
-qcd.add('qcd_600_800','%s'%(fileListFromDisk(loc+"QCD_Pt_600to800")),xs=26.9921/scale) #/pb
+qcd.add('qcd_50_80','%s'%(fileListFromDisk(loc+"QCD_Pt_50to80")),xs=8.148778e6) #/pb
+qcd.add('qcd_80_120','%s'%(fileListFromDisk(loc+"QCD_Pt_80to120")),xs=10.3368e5) #/pb
+qcd.add('qcd_120_170','%s'%(fileListFromDisk(loc+"QCD_Pt_120to170")),xs=15.62933e4) #/pb
+qcd.add('qcd_170_300','%s'%(fileListFromDisk(loc+"QCD_Pt_170to300")),xs=3.413815e4) #/pb
+qcd.add('qcd_300_470','%s'%(fileListFromDisk(loc+"QCD_Pt_300to470")),xs=17.59549e2) #/pb
+qcd.add('qcd_470_600','%s'%(fileListFromDisk(loc+"QCD_Pt_470to600")),xs=113.8791) #/pb
+qcd.add('qcd_600_800','%s'%(fileListFromDisk(loc+"QCD_Pt_600to800")),xs=26.9921) #/pb
 
 '''
 qcd.add('qcd_50_80','%s'%(fileListFromDisk(loc+"QCD_Pt_50to80")),xs=1) #/pb
