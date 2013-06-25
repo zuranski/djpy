d1f05e792ca45adea0a546cc0d4eb968b1978be7
Andrzej Zuranski <zuranski@della3.(none)>
2013-06-25 09:33:10 -0400

    new ntuples

--- samples/__sigmc__.py
+++ samples/__sigmc__.py
@@ -24,18 +24,17 @@ sigmc.add('H_400_X_50b','%s/%s/%s")'%(pnfs,loc,"MH_400_MX_50_Ctau_80b/") ,xs=500
 sigmc.add('H_200_X_50b','%s/%s/%s")'%(pnfs,loc,"MH_200_MX_50_Ctau_200b/") ,xs=500) #/pb
 '''
 
-scale=1
 # PU
-loc='/scratch/gpfs/zuranski/data/DJTuple53x_DEV/'
+loc='/scratch/gpfs/zuranski/data/DJTuple53x_PROD/'
 
-sigmc.add('H_1000_X_350','%s'%(fileListFromDisk(loc+"MH_1000_MX_350_Ctau_35To3500/")) ,xs=1*scale) #/pb
-sigmc.add('H_1000_X_150','%s'%(fileListFromDisk(loc+"MH_1000_MX_150_Ctau_10To1000/")) ,xs=1*scale) #/pb
-sigmc.add('H_1000_X_50','%s'%(fileListFromDisk(loc+"MH_1000_MX_50_Ctau_4To400/")) ,xs=1*scale) #/pb
-sigmc.add('H_1000_X_20','%s'%(fileListFromDisk(loc+"MH_1000_MX_20_Ctau_1p5To150/")) ,xs=1*scale) #/pb
-sigmc.add('H_400_X_150','%s'%(fileListFromDisk(loc+"MH_400_MX_150_Ctau_40To4000/")) ,xs=1*scale) #/pb
-sigmc.add('H_400_X_50','%s'%(fileListFromDisk(loc+"MH_400_MX_50_Ctau_8To800/")) ,xs=1*scale) #/pb
-sigmc.add('H_400_X_20','%s'%(fileListFromDisk(loc+"MH_400_MX_20_Ctau_4To400/")) ,xs=1*scale) #/pb
-sigmc.add('H_200_X_50','%s'%(fileListFromDisk(loc+"MH_200_MX_50_Ctau_20To2000/")) ,xs=1*scale) #/pb
-sigmc.add('H_200_X_20','%s'%(fileListFromDisk(loc+"MH_200_MX_20_Ctau_7To700/")) ,xs=1*scale) #/pb
-sigmc.add('H_120_X_50','%s'%(fileListFromDisk(loc+"MH_120_MX_50_Ctau_50To5000/")) ,xs=1*scale) #/pb
-sigmc.add('H_120_X_20','%s'%(fileListFromDisk(loc+"MH_120_MX_20_Ctau_13To1300/")) ,xs=1*scale) #/pb
+sigmc.add('H_1000_X_350','%s'%(fileListFromDisk(loc+"MH_1000_MX_350_Ctau_35To3500/")) ,xs=1) #/pb
+sigmc.add('H_1000_X_150','%s'%(fileListFromDisk(loc+"MH_1000_MX_150_Ctau_10To1000/")) ,xs=1) #/pb
+sigmc.add('H_1000_X_50','%s'%(fileListFromDisk(loc+"MH_1000_MX_50_Ctau_4To400/")) ,xs=1) #/pb
+sigmc.add('H_1000_X_20','%s'%(fileListFromDisk(loc+"MH_1000_MX_20_Ctau_1p5To150/")) ,xs=1) #/pb
+sigmc.add('H_400_X_150','%s'%(fileListFromDisk(loc+"MH_400_MX_150_Ctau_40To4000/")) ,xs=1) #/pb
+sigmc.add('H_400_X_50','%s'%(fileListFromDisk(loc+"MH_400_MX_50_Ctau_8To800/")) ,xs=1) #/pb
+sigmc.add('H_400_X_20','%s'%(fileListFromDisk(loc+"MH_400_MX_20_Ctau_4To400/")) ,xs=1) #/pb
+sigmc.add('H_200_X_50','%s'%(fileListFromDisk(loc+"MH_200_MX_50_Ctau_20To2000/")) ,xs=1) #/pb
+sigmc.add('H_200_X_20','%s'%(fileListFromDisk(loc+"MH_200_MX_20_Ctau_7To700/")) ,xs=1) #/pb
+sigmc.add('H_120_X_50','%s'%(fileListFromDisk(loc+"MH_120_MX_50_Ctau_50To5000/")) ,xs=1) #/pb
+sigmc.add('H_120_X_20','%s'%(fileListFromDisk(loc+"MH_120_MX_20_Ctau_13To1300/")) ,xs=1) #/pb
