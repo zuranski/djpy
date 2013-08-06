7236ae09699f47d0a012d2628160690f2393391c
Andrzej Zuranski <zuranski@della3.(none)>
2013-08-06 08:37:04 -0400

    expanded dijetTrue variables, min/max/avg/sum for the two matched jets

--- calculables/GenMatching.py
+++ calculables/GenMatching.py
@@ -24,11 +24,17 @@ class dijetTrue(wrappedChain.calculable):
 		if self.source['realData'] or len(self.source['XpdgId']) == 0: 
 			self.value = [True for i in self.source['dijetIndices']]
 			return
-		self.value = [(self.source[self.var][self.source['dijetIdx1'][i]] +
-		              self.source[self.var][self.source['dijetIdx2'][i]])/(2. if self.avg else 1.)
-                      if self.source['jetTrueLxy'][self.source['dijetIdx1'][i]] ==
-                      self.source['jetTrueLxy'][self.source['dijetIdx2'][i]] != None else None
-                      for i in self.source['dijetIndices']]
+		self.value = [None for i in self.source['dijetIndices']]
+		for i in self.source['dijetIndices']:
+			Lxy1=self.source['jetTrueLxy'][self.source['dijetIdx1'][i]]
+			Lxy2=self.source['jetTrueLxy'][self.source['dijetIdx2'][i]]
+			if Lxy1 is None or Lxy2 is None or Lxy1!=Lxy2: continue
+			val1=self.source[self.var][self.source['dijetIdx1'][i]]
+			val2=self.source[self.var][self.source['dijetIdx2'][i]]
+			self.value[i]={'avg':(val1+val2)/2.,
+						   'min':min(val1,val2),
+ 						   'max':max(val1,val2),
+						   'sum':(val1+val2)}[self.calc]
 
 #  special case
 class genqXPt(wrappedChain.calculable):
@@ -46,12 +52,17 @@ class jetTrueNLep(jetTrue):	var='genqNLep'
 class jetTrueBlxyz(jetTrue): var='genqBlxyz'
 class jetTrueXPt(jetTrue): var='genqXPt'
 class jetTrueHPt(jetTrue): var='genqHPt'
+class jetTrueIP2d(jetTrue): var='genqIP2D'
+class jetTrueIP3d(jetTrue): var='genqIP3D'
 
-class dijetTrueLxy(dijetTrue): var='jetTrueLxy'; avg=True
-class dijetTrueCtau(dijetTrue):	var='jetTrueCtau'; avg=True
-class dijetTrueFlavor(dijetTrue): var='jetTrueFlavor'; avg=True
-class dijetTrueNLep(dijetTrue):	var='jetTrueNLep'; avg=False
-class dijetTrueBlxyz(dijetTrue): var='jetTrueBlxyz'; avg=True
-class dijetTrueXPt(dijetTrue): var='jetTrueXPt'; avg=True
-class dijetTrueHPt(dijetTrue): var='jetTrueHPt'; avg=True
-
+class dijetTrueLxy(dijetTrue): var='jetTrueLxy'; calc='avg'
+class dijetTrueCtau(dijetTrue):	var='jetTrueCtau'; calc='avg'
+class dijetTrueFlavor(dijetTrue): var='jetTrueFlavor'; calc='avg'
+class dijetTrueNLep(dijetTrue):	var='jetTrueNLep'; calc='sum'
+class dijetTrueBlxyz(dijetTrue): var='jetTrueBlxyz'; calc='avg'
+class dijetTrueXPt(dijetTrue): var='jetTrueXPt'; calc='avg'
+class dijetTrueHPt(dijetTrue): var='jetTrueHPt'; calc='avg'
+class dijetTrueIP2dMin(dijetTrue): var='jetTrueIp2d'; calc='min'
+class dijetTrueIP2dMax(dijetTrue): var='jetTrueIp2d'; calc='max'
+class dijetTrueIP3dMin(dijetTrue): var='jetTrueIp3d'; calc='min'
+class dijetTrueIP3dMax(dijetTrue): var='jetTrueIp3d'; calc='max'
