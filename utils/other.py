16a35cb07ce94aa2762c2b5ed0e33e7af78fee63
Andrzej Zuranski <zuranski@della3.(none)>
2013-08-06 12:16:19 -0400

    moved removeLowStats function to utils

--- utils/other.py
+++ utils/other.py
@@ -16,3 +16,11 @@ def weightedAvg(x,w):
     err2 = 1./sum([1/pow(wi,2) for wi in w])
     avg = sum(xi/pow(wi,2) for xi,wi in zip(x,w))*err2
     return avg,math.sqrt(err2)
+
+def removeLowStats(histos,relErrMax=1):
+	for histo in histos:
+		for i in range(1,histo.GetNbinsX()+1):
+			if histo.GetBinContent(i)==0: continue
+			if histo.GetBinError(i)/histo.GetBinContent(i)>relErrMax:
+				histo.SetBinContent(i,0)
+				histo.SetBinError(i,0)
