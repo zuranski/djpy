f0f5e948c8ae6fa237e84fa3280541ddfd806fa0
Andrzej Zuranski <zuranski@della3.(none)>
2013-08-12 15:40:35 -0400

    indent change

--- calculables/Event.py
+++ calculables/Event.py
@@ -4,6 +4,6 @@ class PfHt(wrappedChain.calculable):
 	def update(self,ignored):
 		self.value = sum([pt for pt in self.source['jetPt']])
 
-class nPfJets (wrappedChain.calculable):
+class nPfJets(wrappedChain.calculable):
 	def update(self,ignored):
 		self.value = len(self.source["jetPt"])
