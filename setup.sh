d83ba1be5602fff62e254b3c39d6303a4819b5bd
Andrzej Zuranski <zuranski@princeton.edu>
2014-08-13 11:57:34 -0400

    new python & root version

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
-
-export PATH=/home/zuranski/soft/Python-2.7.3/bin:$PATH
-export LD_LIBRARY_PATH=/home/zuranski/soft/Python-2.7.3/lib:$LD_LIBRARY_PATH
 . /home/zuranski/soft/root-v5-34/bin/thisroot.sh
-
 export PYTHONPATH=$PYTHONPATH:`pwd`
 export PATH=$PATH:`pwd`/supy/bin
